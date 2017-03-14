b7c4e2012d6a47c4db225171cc828c61db381121
simon <simonepi@openmailbox.org>
2017-03-14 13:02:10 +0100

    Update bitcoin_ticker.py

--- bitcoin_ticker.py
+++ bitcoin_ticker.py
@@ -8,12 +8,13 @@ import requests
 import time
 
 f = open("dataset.csv","a")
+keys = ["price_usd","24h_volume_usd","market_cap_usd","available_supply","total_supply","percent_change_1h","percent_change_24h","percent_change_7d"]
 
 while True:
-  data = requests.get("https://api.blockchain.info/stats").json()
+  data = requests.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/").json()[0]
   price = requests.get("https://btc-e.com/api/3/ticker/btc_usd").json()
   for i in data.keys():
-    if "{}".format(i) != "timestamp" and "{}".format(i) != "market_price_usd":
+    if i in keys:
       f.write(str(data[i])+",")
   f.write("{},{},{}".format(price["btc_usd"]["high"],price["btc_usd"]["low"],price["btc_usd"]["avg"]))
   f.write("\n")
