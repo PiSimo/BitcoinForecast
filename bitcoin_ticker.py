3a0a1e589544e1fe7c8fd35567229f262d267def
simon <simonepi@openmailbox.org>
2017-03-16 21:00:37 +0100

    Update bitcoin_ticker.py

--- bitcoin_ticker.py
+++ bitcoin_ticker.py
@@ -12,11 +12,14 @@ keys = ["price_usd","24h_volume_usd","market_cap_usd","available_supply","total_
 
 while True:
   data = requests.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/").json()[0]
-  price = requests.get("https://btc-e.com/api/3/ticker/btc_usd").json()
+#  price = requests.get("https://btc-e.com/api/3/ticker/btc_usd").json()
+  bstamp = requests.get("https://www.bitstamp.net/api/v2/ticker/btcusd/").json() 
+  bkc = requests.get("https://blockchain.info/ticker").json()
   for i in data.keys():
     if i in keys:
       f.write(str(data[i])+",")
-  f.write("{},{},{}".format(price["btc_usd"]["high"],price["btc_usd"]["low"],price["btc_usd"]["avg"]))
+  f.write("{},{},".format(bstamp["volume"],bstamp["vwap"]))
+  f.write("{},{},{}".format(bkc["USD"]["sell"],bkc["USD"]["buy"],bkc["USD"]["15m"]))
   f.write("\n")
   f.flush()
-  time.sleep(5*60)
+  time.sleep(9*60)
