350d9ad26dce5d74e0623a881397fd1489259d80
simon <simonepi@openmailbox.org>
2017-03-17 11:43:26 +0100

    Update network.py

--- network.py
+++ network.py
@@ -15,10 +15,10 @@ def main():
     #Building network
     print("Building net..",end="")
     net = Sequential()
-    net.add(Dense(12,input_dim=12,activation='linear'))
-    net.add(Reshape((1,12)))
+    net.add(Dense(leng,input_dim=leng,activation='linear'))
+    net.add(Reshape((1,leng)))
     net.add(GRU(30,activation='relu',return_sequences=True))
-    #net.add(Dropout(0.3))
+    net.add(Dropout(0.3))
     net.add(GRU(49,activation='sigmoid',return_sequences=False))
     net.add(Dense(1,activation='sigmoid'))
     net.compile(optimizer=SGD(lr=.01,momentum=.9,nesterov=True),loss='mse')
@@ -26,22 +26,23 @@ def main():
 
     #Data
     print("Loading data...",end="")
-    f = open(file_name,'r')
-    data,labels = util.loadData(f)
-    data = util.reduceMatRows(data)
-    labels,m1,m2 =util.reduceVector(labels,getVal=True)
-    print("{} chunk loaded!\nTraining...".format(len(labels)),end="")
+    f            = open(file_name,'r')
+    data,labels  = util.loadData(f)
+    data         = util.reduceMatRows(data)
+    labels,m1,m2 = util.reduceVector(labels,getVal=True)
+    print("{} chunks loaded!\nTraining...".format(len(labels)),end="")
 
     #Training dnn
     net.fit(data,labels,nb_epoch=250)
 
     print("trained!")
 
-    for i in range(len(data)):
-        x = np.array(data[i]).reshape(1,12)
-        predicted = util.augmentValue(net.predict(x)[0],m1,m2)
-        real = util.augmentValue(labels[i],m1,m2)
-        if i > 80:print("Real:{} Predicted:{}  likehood({})".format(real,predicted,abs( math.sqrt((real-predicted)**2) )))
+    for i in range(len(data)): #Train all over the dataset to give weights to the RNN unit
+        x         = np.array(data[i]).reshape(1,12)       #Reshape input data to forward it inside the dnn
+        predicted = util.augmentValue(net.predict(x)[0],m1,m2) #Denormalize data 
+        real      = util.augmentValue(labels[i],m1,m2)    #Denormalize data
+        lhood     = abs( math.sqrt((real-predicted)**2))  #Calculate distance between predicted and real
+        if i > (len(data)-20):print("Real:{} Predicted:{}  likehood({})".format(real,predicted,  ))
 
 if __name__ == '__main__':
     main()
