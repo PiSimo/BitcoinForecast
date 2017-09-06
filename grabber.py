f1959939901d25cd61b7ff04b948c397b6b063a3
simon <simonepi@openmailbox.org>
2017-09-06 09:18:17 +0200

    Update grabber.py
    fixed order problem

--- grabber.py
+++ grabber.py
@@ -10,14 +10,20 @@ import time
 f_name = input("dataset name:")
 f = open(f_name,"a")
 keys = ["price_usd","24h_volume_usd","market_cap_usd","available_supply","total_supply","percent_change_1h","percent_change_24h","percent_change_7d"]
+vals = [0]*len(keys)
 
 while True:
   data = requests.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/").json()[0]
   bstamp = requests.get("https://www.bitstamp.net/api/v2/ticker/btcusd/").json() 
   bkc = requests.get("https://blockchain.info/ticker").json()
-  for i in data.keys():
-    if i in keys:
-      f.write(str(data[i])+",")
+  
+  for d in data.keys():
+     if d in keys:
+       indx = keys.index(d)
+       vals[indx] = data[d]
+  for val in vals:
+       f.write(val+",")
+      
   f.write("{},{},".format(bstamp["volume"],bstamp["vwap"]))
   f.write("{},{},{}".format(bkc["USD"]["sell"],bkc["USD"]["buy"],bkc["USD"]["15m"]))
   f.write("\n")
